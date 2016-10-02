2541f343816e57d13be9cf5496a40ebd412911a1
Mohammad Albakri <mohammad.albakri93@gmail.com>
2016-10-02 12:36:16 +0300

    Added two new API routes

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,6 +1,7 @@
 var shopController = require('./../Workshop/shopController.js');
 
-
 module.exports = function(app){
-	app.get('/api/all/shops', shopController.all);
+	app.get('/api/all/shops', shopController.getAllShops);
+	app.post('/api/add/new/shop', shopController.addNewShop);
+	app.get('/api/get/one/:id', shopController.getOneShop);
 }
