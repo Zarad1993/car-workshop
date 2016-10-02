b4915a81f7c52fcd56b8733af387722317b43816
Mohammad Albakri <mohammad.albakri93@gmail.com>
2016-10-02 12:35:24 +0300

    Added two new main methods for our shop controller

--- server/Workshop/shopController.js
+++ server/Workshop/shopController.js
@@ -1,5 +1,27 @@
+var Shop = require('./shopModel.js');
 module.exports = {
-	all : function(req,res){
-		res.status(200).send('Gotta Catch Em All');
+	getAllShops : function(req,res){
+		Shop.find({})
+			.exec(function(err,shops){
+				return shops.length > 0 ? res.status(200).send(shops) : res.status(500).send('No shops Available');
+			})
+	}, 
+	addNewShop : function(req,res){
+		var newShop = new Shop({
+			name : req.body.name,
+			found : req.body.date,
+			phone : req.body.phone,
+			city : req.body.city,
+			category : req.body.category
+		})
+		newShop.save(function(err, saved){
+			return saved ? res.status(201).send('New Shop Successfully Added') : res.status(500).send('Error Saving Shop');
+		})
+	},
+	getOneShop : function(req,res){
+		Shop.findOne({_id : req.params.id})
+			.exec(function(err, shop){
+				return shop ? res.status(200).send(shop) : res.status(500).send('Error Getting one Shop');
+			})
 	}
-}
+};
